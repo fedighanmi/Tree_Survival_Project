17db9010c22a9f301e352d079be3431e8a50c35b
Fedi Ghanmi <78984143+fedighanmi@users.noreply.github.com>
2024-01-17 14:30:30 +0100

    Create DataPreprocessor function.

--- Tree-Project/preprocessing.py
+++ Tree-Project/preprocessing.py
@@ -1,2 +1,15 @@
+from sklearn.preprocessing import MinMaxScaler
 
+
+class DataPreprocessor:
+
+    def __init__(self, data):
+        self.data = data.copy()
+        self.scaler = MinMaxScaler()
+
+    def normalize_data(self, numeric_columns):
+        numeric_data = self.data[numeric_columns]
+        normalized_data = self.scaler.fit_transform(numeric_data)
+        self.data[numeric_columns] = normalized_data
+        return self.data
 
