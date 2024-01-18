1ac0ee2540a2f85a9cd6c5546bfd934a15afe6d7
Sarvin23 <sarvingunasekar717@gmail.com>
2024-01-18 22:27:12 +0100

    Update the code Cleaning.py

--- Tree-Project/Cleaning.py
+++ Tree-Project/Cleaning.py
@@ -2,6 +2,14 @@ import pandas as pd
 
 data = pd.read_csv('../example/Tree_Data.csv')
 
-# Check for missing values in each column
-print(data.isnull().sum())
+# Data Cleaning and Feature Engineering
+data = data.drop(['Plot', 'Subplot', 'Time', 'Event', 'Harvest'], axis=1)
 
+# Handle missing values
+data['EMF'].fillna(data['EMF'].mean(), inplace=True)
+
+# Check for any null values
+data.any().isnull()
+
+# Remove duplicate rows in the dataset
+data = data.drop_duplicates()
