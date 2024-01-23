2e4872fea1e69beff9c3d6647bd44151c940dbca
Fedi Ghanmi <78984143+fedighanmi@users.noreply.github.com>
2024-01-23 14:39:07 +0100

    Modify setup.py file

--- setup.py
+++ setup.py
@@ -1,14 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='tree_lab',
-    version='0.1.0',
-    packages=find_packages(),  # Automatically discover and include all packages
-    install_requires=[
-        # List your dependencies here
-        'numpy',
-        'pandas',
-        'polars',
-        # Add other dependencies as needed
-    ],
-)
+setup()
