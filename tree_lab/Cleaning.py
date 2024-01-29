9f8c40ee6949bd632484a1bcd4b2184fe11d84db
Fedi Ghanmi <78984143+fedighanmi@users.noreply.github.com>
2024-01-29 17:00:29 +0100

    Refactor code in Cleaning.py and add documentation.

--- tree_lab/Cleaning.py
+++ tree_lab/Cleaning.py
@@ -1,13 +1,20 @@
-import pandas as pd
-
-data = pd.read_csv('../example/Tree_Data.csv')
-
-
 class TreeDataCleaner:
+    """
+    Initializes a TreeDataCleaner object.
+
+    Parameters:
+        data (pandas.DataFrame): The input pandas DataFrame containing tree data.
+    """
+
     def __init__(self, data):
-        self.data = data
+        self.data = data.copy()
 
     def detect_na(self):
+        """
+        Checks for null values in the dataset, prints columns with null values (if any),
+        and removes duplicate rows in the dataset.
+        """
+
         # Check for any null values in the dataset
         null_values_check = self.data.isnull().any()
 
@@ -20,16 +27,28 @@ class TreeDataCleaner:
         self.data = self.data.drop_duplicates()
 
     def impute_na(self):
+        """
+        Imputes missing values in the 'EMF' column by filling them with the mean of the column.
+        Fills any remaining null values in the dataset with 0.
+        """
+
         self.data['EMF'].fillna(self.data['EMF'].mean(), inplace=True)
         self.data = self.data.fillna(0)
 
     def modify_status(self):
+        """
+        Modifies the 'Alive' column by replacing 'X' with 1.
+        Renames the 'Event' column to 'Dead'.
+
+        Returns:
+            pandas.DataFrame: The modified DataFrame.
+        """
         self.data['Alive'] = self.data['Alive'].replace('X', 1)
         self.data = self.data.rename(columns={'Event': 'Dead'})
 
         return self.data
 
-    def input_values(self, columns_to_delete):
+    def del_cols(self, columns_to_delete):
         """
         Function to allow users to delete specified columns.
 
@@ -37,13 +56,22 @@ class TreeDataCleaner:
         - columns_to_delete: A list of column names to be deleted.
 
         Example:
-        cleaner.input_values(['Age', 'Height'])
+        cleaner.del_cols(['Age', 'Height'])
         """
+
         for column_name in columns_to_delete:
             if column_name in self.data.columns:
                 del self.data[column_name]
             else:
-                print(f"Column '{column_name}' not found in the dataset.")
+                print(f"Column '{column_name}' either already deleted or not found in the dataset.")
+
+
+    def display(self):
+        """
+        Returns the current state of the data.
+
+        Returns:
+        pandas.DataFrame: The current state of the data.
+        """
 
         return self.data
-
