243216d808580809bc9a635d1dbe9e96e55393c8
Fedi Ghanmi <78984143+fedighanmi@users.noreply.github.com>
2024-01-30 15:51:32 +0100

    Refactor comments.

--- tree_lab/Cleaning.py
+++ tree_lab/Cleaning.py
@@ -67,7 +67,6 @@ class TreeDataCleaner:
 
     def display(self):
         """
-        Returns the current state of the data.
 
         Returns:
         pandas.DataFrame: The current state of the data.
