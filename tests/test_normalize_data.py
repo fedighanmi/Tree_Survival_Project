d83e254a0e9b35f91805f9f892f1b1fdd4e96b36
Fedi Ghanmi <78984143+fedighanmi@users.noreply.github.com>
2024-02-04 22:43:01 +0100

    Test the file test_normalize_data.py

--- tests/test_normalize_data.py
+++ tests/test_normalize_data.py
@@ -1,5 +1,35 @@
 import pytest
+import pytest
+import pandas as pd
+from tree_lab import preprocessing as prp
 
 def test_normalize():
     pass
+
+
+# Assume you have a function that processes a DataFrame
+def process_data(df):
+    # Some processing logic on two columns
+    df['result'] = df['column1'] + df['column2']
+    return df
+
+
+# Fixture to create a sample DataFrame
+@pytest.fixture
+def sample_data():
+    data = {'column1': [1, 2, 3],
+            'column2': [4, 5, 6]}
+    return pd.DataFrame(data)
+
+
+# Test case to check if the result column is calculated correctly
+def test_process_data(sample_data):
+    processed_data = process_data(sample_data)
+    l = [5, 7, 9]
+    # Check if the 'result' column is calculated correctly
+    assert processed_data['result'].tolist() == pytest.approx(l, abs=1e-2)
+
+
+
+
 
